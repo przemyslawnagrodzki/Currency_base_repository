d5517d3c5487b5d21e2d90bbdb5dbf6287bcfb34
Przemysław Nagrodzki <nagrodzkiprzemyslaw@gmail.com>
2023-04-06 13:58:56 +0200

    Start project

---
+++ src/components/ResultBox/ResultBox.js
@@ -0,0 +1,37 @@
+import PropTypes from 'prop-types';
+import { convertUSDToPLN } from './../../utils/convertUSDToPLN';
+import { convertPLNToUSD } from './../../utils/convertPLNToUSD';
+import { formatAmountInCurrency } from './../../utils/formatAmountInCurrency';
+import { useMemo } from 'react';
+import styles from './ResultBox.module.scss';
+
+const ResultBox = ({ from, to, amount }) => {
+
+  const convertedAmount = useMemo(() => {
+    if (from === 'USD' && to === 'PLN') return convertUSDToPLN(amount);
+    if (from === 'PLN' && to === 'USD') return convertPLNToUSD(amount);
+    if (typeof amount === 'string') { return NaN }
+    if (amount === '') { return NaN }
+    if (typeof amount !== 'string' && typeof amount !== 'number') { return 'Error' }
+    if (amount < 0) { return '$0.00' }
+    return formatAmountInCurrency(amount, from);
+  }, [from, to, amount]);
+
+
+
+  const formattedAmount = useMemo(() => formatAmountInCurrency(amount, from), [amount, from]);
+
+  return (
+    <div className={styles.result}>
+      {formattedAmount} = {convertedAmount}
+    </div>
+  );
+};
+
+ResultBox.propTypes = {
+  from: PropTypes.string.isRequired,
+  to: PropTypes.string.isRequired,
+  amount: PropTypes.number.isRequired,
+}
+
+export default ResultBox;
